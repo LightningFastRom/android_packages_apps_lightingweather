2900cba89b1dd13257debc79339813e4adbb7e81
DJABHipHop <castellanos.kevin@rocketmail.com>
2019-07-04 12:36:24 -0400

    Fixing LauncherActivity.java X
    Change-Id: I68fce495d5c9b03e86a52f8590fab9def20665a5

--- app/src/main/java/xyz/fancystuff/weatherlauncher/LauncherActivity.java
+++ app/src/main/java/xyz/fancystuff/weatherlauncher/LauncherActivity.java
@@ -1,23 +1,24 @@
-package xyz.fancystuff.weatherlauncher
+package xyz.fancystuff.weatherlauncher;
 
-import android.app.Activity
-import android.content.ComponentName
-import android.net.Uri
-import android.os.Bundle
-import android.widget.Toast
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Toast;
 
-class LauncherActivity : Activity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        try {
+public class LauncherActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		try {
 			Intent intent = new Intent(); 
-            intent.setClassName("com.google.android.googlequicksearchbox", "com.google.android.apps.gsa.velour.DynamicActivityTrampoline")
-            intent.data = Uri.parse("dynact://velour/weather/ProxyActivity")
-            startActivity(intent)
-        }
-        catch (e: Exception) {
-            Toast.makeText(this, getString(R.string.no_google_weather), Toast.LENGTH_LONG).show()
-        }
-        finish()
+			intent.setClassName("com.google.android.googlequicksearchbox", "com.google.android.apps.gsa.velour.DynamicActivityTrampoline");
+			intent.setData(Uri.parse("dynact://velour/weather/ProxyActivity"));
+			startActivity(intent);
+		} catch (Exception e) {
+			Toast.makeText(this, getString(R.string.no_google_weather), Toast.LENGTH_LONG).show();
+		}
+		finish();
     }
 }
